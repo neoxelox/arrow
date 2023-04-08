59992256b727f282e0c5b1eef22f74fe5e32bd8e
Alex <alex@neoxelox.com>
2023-04-08 17:34:38 +0200

    Update components

--- src/components/Toast.svelte
+++ src/components/Toast.svelte
@@ -16,7 +16,11 @@
 </script>
 
 <div
-  class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+  class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-light shadow-lg ring-1 ring-dark-900 ring-opacity-5 border"
+  class:border-success-400={type === ToastType.SUCCESS}
+  class:border-info-400={type === ToastType.INFO}
+  class:border-warning-500={type === ToastType.WARNING}
+  class:border-danger-400={type === ToastType.ERROR}
   in:classes={{
     duration: 350,
     base: "transform ease-out duration-300 transition",
@@ -24,36 +28,36 @@
     to: "translate-y-0 opacity-100 sm:translate-x-0",
   }}
   out:classes={{
-    duration: 100,
-    base: "transition ease-in duration-100",
-    from: "opacity-100",
-    to: "opacity-0",
+    duration: 350,
+    base: "transform ease-out duration-300 transition",
+    from: "translate-y-0 opacity-100 sm:translate-x-0",
+    to: "translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2",
   }}
 >
   <div class="p-4">
     <div class="flex items-start">
       <div class="flex-shrink-0">
         {#if type === ToastType.SUCCESS}
-          <CheckCircle class="h-6 w-6 text-green-400" />
+          <CheckCircle class="h-6 w-6 text-success-400" />
         {:else if type === ToastType.INFO}
-          <InformationCircle class="h-6 w-6 text-blue-400" />
+          <InformationCircle class="h-6 w-6 text-info-400" />
         {:else if type === ToastType.WARNING}
-          <QuestionMarkCircle class="h-6 w-6 text-yellow-400" />
+          <QuestionMarkCircle class="h-6 w-6 text-warning-500" />
         {:else if type === ToastType.ERROR}
-          <ExclamationCircle class="h-6 w-6 text-red-400" />
+          <ExclamationCircle class="h-6 w-6 text-danger-400" />
         {/if}
       </div>
       <div class="ml-3 w-0 flex-1 pt-0.5">
         {#if type === ToastType.SUCCESS}
-          <p class="text-sm font-medium text-gray-900">Action successful!</p>
-          <p class="mt-1 text-sm text-gray-500">{message}</p>
+          <p class="text-sm font-medium text-dark-900">Action successful!</p>
+          <p class="mt-1 text-sm text-dark-500">{message}</p>
         {:else if type === ToastType.INFO}
-          <p class="text-sm font-medium text-gray-900">{message}</p>
+          <p class="text-sm font-medium text-dark-900">{message}</p>
         {:else if type === ToastType.WARNING}
-          <p class="text-sm font-medium text-gray-900">{message}</p>
+          <p class="text-sm font-medium text-dark-900">{message}</p>
         {:else if type === ToastType.ERROR}
-          <p class="text-sm font-medium text-gray-900">An error has ocurred!</p>
-          <p class="mt-1 text-sm text-gray-500">{message}</p>
+          <p class="text-sm font-medium text-dark-900">An error has ocurred!</p>
+          <p class="mt-1 text-sm text-dark-500">{message}</p>
         {/if}
       </div>
 
@@ -61,7 +65,7 @@
         <div class="ml-4 flex flex-shrink-0">
           <button
             type="button"
-            class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            class="inline-flex rounded-md bg-light text-dark-400 hover:text-dark-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
             on:click={() => dispatch("dismiss")}
           >
             <span class="sr-only">Dismiss</span>
---
+++ src/components/icons/144/solid/Logo.svelte
@@ -0,0 +1,8 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144 144" fill="currentColor" aria-hidden="true" {...$$props}>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M144 72C144 111.765 111.765 144 72 144C32.2355 144 0 111.765 0 72C0 32.2355 32.2355 0 72 0C111.765 0 144 32.2355 144 72ZM72 121C99.062 121 121 99.062 121 72C121 44.938 99.062 23 72 23C44.938 23 23 44.938 23 72C23 99.062 44.938 121 72 121Z"
+  />
+  <circle cx="72" cy="72" r="28" />
+</svg>
